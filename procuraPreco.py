e9bdf730f9bc90dcfdb572994b22073a59416637
Rafael Rebello Vitorino <rrebellov@gmail.com>
2022-10-01 22:59:18 -0300

    formatação

--- procuraPreco.py
+++ procuraPreco.py
@@ -2,21 +2,24 @@ import urllib.request
 import time
 import smtplib
 
+
 def criarMensagem():
-    body = f'''Subject: Compre Cafe
+    body = f"""Subject: Compre Cafe
 
 Os precos estao abaixo de $4.70, compre.
 Preco obtido em {dia}/{mes} as {hora}:{minuto}:{segundo}
-Mensagem automatica'''
+Mensagem automatica"""
     return body
 
+
 def obterLogin():
-    with open('enderecoEmail.txt', 'r') as file:
-        return file.read().split(' ')
+    with open("enderecoEmail.txt", "r") as file:
+        return file.read().split(" ")
+
 
 def obterDestinos():
     destinos = []
-    with open('destinos.txt', 'r') as f:
+    with open("destinos.txt", "r") as f:
         for line in f:
             destinos.append(line.strip())
     return destinos
@@ -28,14 +31,15 @@ def mensagem():
     corpo = criarMensagem()
     print("Enviando mensagem")
 
-    smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587) 
+    smtpObj = smtplib.SMTP("smtp-mail.outlook.com", 587)
     smtpObj.ehlo()
     smtpObj.starttls()
-    smtpObj.login(mail, senha) 
-    smtpObj.sendmail(mail, destinos, corpo) 
+    smtpObj.login(mail, senha)
+    smtpObj.sendmail(mail, destinos, corpo)
     smtpObj.quit()
 
-def receberPreco(site): #retorna float com o preço
+
+def receberPreco(site):  # retorna float com o preço
     pagina = urllib.request.urlopen(site)
     texto = pagina.read().decode("utf8")
     indexacao = (texto.find("$")) + 1
@@ -48,7 +52,9 @@ while True:
     precoNormal = receberPreco("http://beans.itcarlow.ie/prices.html")
     precoEspecial = receberPreco("http://beans.itcarlow.ie/prices-loyalty.html")
 
-    mes, dia, hora, minuto, segundo = time.localtime()[1:6]  #obtém a hora em que a consulta foi feita
+    mes, dia, hora, minuto, segundo = time.localtime()[
+        1:6
+    ]  # obtém a hora em que a consulta foi feita
 
     print(f"Preço normal: {precoNormal}  Preço Especial: {precoEspecial} ")
     print(f"Preço obtido em {dia}/{mes}  {hora}:{minuto}:{segundo}")
@@ -58,6 +64,5 @@ while True:
     if (precoNormal < 4.70) or (precoEspecial < 4.70):
         mensagem()
         break
-
 
     time.sleep(2)
